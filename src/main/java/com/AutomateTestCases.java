396570a25ea12c0e991af08431893b299f37047a
madhanvz1991 <madhanswamy17@gmail.com>
2015-08-31 17:16:31 +0530

    Create AutomateTestCases.java

---
+++ src/main/java/com/AutomateTestCases.java
@@ -0,0 +1,22 @@
+package com;
+
+public class AutomateTestCases {
+
+	public int oprAdd(int a, int b){
+        return a + b;
+    }
+	
+	public int oprSub(int a, int b){
+        return a - b;
+    }
+    
+    public int oprMul(int a, int b){
+        return a * b;
+    }
+    
+    public String oprStr(String a, String b){
+        return b;
+    }
+	
+	
+}
